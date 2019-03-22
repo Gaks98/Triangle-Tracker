e4cd8f89d884d5f49ff0b2e501761fff9f433193
yvonne <yvonnegax98@gmail.com>
2019-03-22 14:29:11 +0300

    first structure having first complete html and js.

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+function triangle (sideA,sideB,sideC) {
+    var sideA = prompt("Enter a value for side A: ");
+    var sideB = prompt("Enter a value for side B: ");
+    var sideC = prompt("Enter a value for side A: ");
+
+    var a = parseInt(sideA);
+    var b = parseInt(sideB);
+    var c = parseInt(sideC);
+
+    if (a===b && b===c && c===a) {
+        alert("Equilateral");
+    }
+    else if (a===b && a!==c && b!==c || b===c && b!==a && c!==a || a===c && a!==b && b!==c) {
+        alert("isosceles");
+    }
+    else if(a!==b && b!==c && c!==a)  {
+        alert("scalene");
+    }
+    else{
+        alert("Not a Number");
+    }
+}
+triangle();
+
+
+if (isNaN(sideA) || isNaN(sideB) || isNaN(sideC)) {
+    triType.textContent = "Please enter numbers only!";
+}
---
+++ js/script2.js
@@ -0,0 +1,24 @@
+function triangle(){
+    var sideA = parseFloat(document.getElementById("A").value);
+    var sideB = parseFloat(document.getElementById("B").value);
+    var sideC = parseFloat(document.getElementById("C").value);
+    var triType = document.getElementById("triType");
+
+    if ((sideA===sideB && sideB===sideC)){
+        var equilateral = "equilateral";
+        triType.textContent = "Triangle : " + equilateral;
+    }
+    else if (sideA+sideB <= sideC || sideA+sideC <= sideB || sideB+sideC <= sideA ) {
+        triType.textContent = "Not a Triangle! the sum of any two sides of the triangle must be greater than the third side.Try again";
+    }
+    else if (sideA===sideB && sideA!==sideC && sideB!==sideC || sideB===sideC
+        && sideB!==sideA && sideC!==sideA || sideA===sideC && sideA!==sideB && sideB!==sideC) {
+        var isosceles = "isosceles";
+        triType.textContent = "Triangle : " + isosceles;
+    }
+    else  {
+        var scalene = "scalene";
+        triType.textContent = "Triangle : " + scalene;
+    }
+
+}
